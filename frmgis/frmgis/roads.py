e07a74b7e38f193baaea5f6c05e78cbd1cd310b3
Fergal <fergal.mullally@gmail.com>
2023-05-13 15:17:42 -0400

    Cleanup

--- frmgis/frmgis/roads.py
+++ frmgis/frmgis/roads.py
@@ -22,11 +22,11 @@ This isn't very sophisiticated. There is no way to specify which
 roads you want, or exactly where to put the annotations
 """
 
-def plot_interstate(clr='#FEEFC3', edgecolor='#F3C04F', annotate=True):
+def plot_interstate(clr='#FEEFC3', edgecolor='#F3C04F', lw=3, annotate=True):
     path = os.path.dirname(__file__)
 
     df = load(os.path.join(path, 'data/MarylandInterstates.kml'))
-    plot_roads(df, clr, edgecolor)
+    plot_roads(df, clr, edgecolor, lw=lw)
     if annotate:
         annotate_interstate(path)
               
@@ -54,62 +54,61 @@ def load(fn):
     return df 
 
 
-def plot_roads(df, clr, edgecolor):
-    effect = fplots.outline(clr=edgecolor, lw=5)
+def plot_roads(df, clr, edgecolor, lw=3):
+    effect = fplots.outline(clr=edgecolor, lw=1.6*lw)
     for g in df.geom:
-        fgplots.plot_shape(g, '-', lw=3, color=clr, path_effects=effect)
+        fgplots.plot_shape(g, '-', lw=lw, color=clr, path_effects=effect)
 
 
 
 
 def annotate_interstate(path):
-    # path = os.path.dirname(__file__)
-    # path = "/home/fergal/data/elections/shapefiles/mdroads/I-{num}.png"
-    path = os.path.join(path, "data/I-{num}.png")
     num = 70
     coords = np.array([
         [-76.803, 39.298]
     ])
-    plot_road_markers(coords[:,0], coords[:,1], num)
+    plot_road_markers(path, coords[:,0], coords[:,1], num)
 
     num = 83
     coords = np.array([
-        [-76.665, 39.537],
+        [-76.6464, 39.680],
+        # [-76.665, 39.537],
         [-76.647, 39.337],
     ])
-    plot_road_markers(coords[:,0], coords[:,1], num)
+    plot_road_markers(path, coords[:,0], coords[:,1], num)
 
     num = 95
     coords = np.array([
         [-76.380, 39.438],
         [-76.732, 39.214],
     ])
-    plot_road_markers(coords[:,0], coords[:,1], num)
+    plot_road_markers(path, coords[:,0], coords[:,1], num)
 
     num = 695
     coords = np.array([
-        [-76.557, 39.400]
+        # [-76.557, 39.400],
+        [-76.7078, 39.3953],
     ])
-    plot_road_markers(coords[:,0], coords[:,1], num)
+    plot_road_markers(path, coords[:,0], coords[:,1], num)
 
     num = 795
     coords = np.array([
         [-76.826, 39.447]
     ])
-    plot_road_markers(coords[:,0], coords[:,1], num)
+    plot_road_markers(path, coords[:,0], coords[:,1], num)
 
 
-def plot_road_markers(x, y, rt_number, ax=None):
+def plot_road_markers(path, x, y, rt_number):
     """
     stolen from 
     https://stackoverflow.com/questions/2318288/how-to-use-custom-png-image-marker-with-plot
     """
     rt_number = str(rt_number)
-    #TODO Make more general!
-    path = f"/home/fergal/data/elections/shapefiles/mdroads/I-{rt_number}.png"
+
+    path = os.path.join(path, f'data/png/I-{rt_number}.png')
     image = plt.imread(path)
 
-    ax = ax or plt.gca()
+    ax = plt.gca()
     for xi, yi in zip(x,y):
         im = OffsetImage(image, zoom=18/ax.figure.dpi)
         im.image.axes = ax

24e96d16aa6a111f6a13a39553bc554cc63303ac
Fergal <fergal.mullally@gmail.com>
2023-07-29 16:49:20 -0400

    Created an abstract class, and concrete classes for different placement algorithms. Added a new, better algo

--- frmplots/frmplots/platlabels.py
+++ frmplots/frmplots/platlabels.py
@@ -5,10 +5,11 @@ This will be a code to annotate a figure in such a way that labels don't overlap
 It's not there yet
 """
 
-import matplotlib.pyplot as plt 
+from ipdb import set_trace as idebug
+import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import itertools 
+import itertools
 
 """
 if is_visible[j]:
@@ -20,11 +21,11 @@ if is_visible[j]:
             is_overlap = self.overlap(t, self.tList[j])
             if not is_overlap:
                 #Break out of the loop, the label goes here"
-            
+
 """
 
 
-class PlatLabel():
+class AbstractPlatLabel():
     """Hide some overlapping labels on a plot
 
     Given a set of matplotlib.Text objects, figure out which
@@ -107,80 +108,6 @@ class PlatLabel():
         level = kwargs.pop('level', 1)
         self.add(plt.text(x, y, text, **kwargs), level)
 
-    def render(self):
-        """Compute which labels overlap and which ones to hide
-
-        Returns:
-        ------------
-        **None**
-        """
-        levels = np.array( list(map(lambda x: x.level, self.tList)))
-
-        srt = np.argsort(levels)
-        is_visible = np.ones_like(srt, dtype=bool)
-
-        for i in range(len(srt)):
-            t_i = self.tList[ srt[i] ]
-            t_i.set_visible(True)
-
-            j = 0
-            while j < i:
-                t_j = self.tList[ srt[j] ]
-                if self.overlap(t_i, t_j) and is_visible[j]:
-                    # print(f"{i} overlaps with {j}, {t_j}")
-                    # print(f"Marking elt {i} ({t_i}) as invisible")
-                    is_visible[i] = False 
-                    t_i.set_visible(False)
-                    break
-                j+= 1
-
-
-    # def advanced_render(self):
-
-    #     #Level 1 comes before level 2 etc.
-    #     tList = sorted(self.tList, key=lambda x: x.level)
-    #     nLabel = len(tList)
-
-    #     ha = "left right".split()
-    #     va = "bottom top".split()
-    #     placement_options = list(itertools.product(va, ha))
-
-    #     for i in range(nLabel):
-    #         t_i = tList[i]
-    #         t_i.set_visible(False)
-
-    #     for i in range(nLabel):
-    #         t_i = tList[i]
-    #         t_i.set_visible(True)
-
-    #         for pl in placement_options:
-    #             t_i.set_horizontalalignment(pl[1])
-    #             t_i.set_verticalalignment(pl[0])
-    #             print(t_i.get_text(), pl, t_i.get_ha(), t_i.get_va())
-    #             print( getTextBbox(t_i))
-
-    #             flag = True 
-    #             for j in range(i):
-    #                 t_j = tList[j]
-
-    #                 if  t_j.get_visible() and self.overlap(t_i, t_j):
-    #                     flag = False 
-    #                     #We found an overlap, this placement is invalid, stop looking
-    #                     #Try the next placement option
-    #                     break 
-
-    #             plt.pause(.1)
-    #             print(t_i)
-    #             import ipdb; ipdb.set_trace()
-
-    #             if flag:
-    #                 #We found a legimiate placement, stop looking for more
-    #                 #Try to place the label i+1
-    #                 t_i.set_visible(True)
-    #                 break
-    #             t_i.set_visible(False)
-
-
     def overlap(self, t1, t2):
         """Do two text objects overlap?
 
@@ -198,21 +125,116 @@ class PlatLabel():
         """
         c1 = getTextBbox(t1)
         c2 = getTextBbox(t2)
+        # print(c1, c2, c1.intersection(c1,c2))
         return c1.intersection(c1,c2) is not None
 
+    def render(self):
+        """Compute which labels overlap and which ones to hide
 
-    # def drawAllBboxes(self):
-    #     for t in self.tList:
-    #         self.drawBBox(t)
+        Returns:
+        ------------
+        **None**
+        """
+        raise NotImplementedError("Don't call abstract class directly")
+    
 
-    # def drawBbox(self, t, color='k'):
-    #     bbox = getTextBbox(t)
-    #     corners = bbox.corners()
-    #     print(corners)
-    #     for i in range(0, len(corners)):
-    #         x0, y0 = corners[i-1]
-    #         x1, y1 = corners[i]
-    #         plt.plot([x0, x1], [y0,y1], '-', color=color, lw=1)
+class SimplePlatLabel(AbstractPlatLabel):
+    """Put all the labels to the upper right of their points. 
+
+    Hide lower priority labels that overlap
+    """
+    def render(self):
+        levels = np.array( list(map(lambda x: x.level, self.tList)))
+
+        srt = np.argsort(levels)
+        print(srt)
+        is_visible = np.ones_like(srt, dtype=bool)
+
+        for i in range(len(srt)):
+            t_i = self.tList[ srt[i] ]
+            t_i.set_visible(True)
+
+            j = 0
+            while j < i:
+                print(i, j)
+                t_j = self.tList[ srt[j] ]
+                if self.overlap(t_i, t_j) and is_visible[j]:
+                    print(f"{srt[i]} overlaps with {srt[j]}, {t_j}")
+                    print(f"Marking elt {srt[i]} ({t_i}) as invisible")
+                    is_visible[i] = False
+                    t_i.set_visible(False)
+                    break
+                j+= 1
+
+
+
+
+class PlatLabel(AbstractPlatLabel):
+    """Try different options for ha and va before hiding label"""
+
+    def __init__(self):
+        PlatLabel.__init__(self)
+
+    def render(self):
+
+        # Get priority order of labels
+        tList = sorted(self.tList, key=lambda x: x.level)
+        nLabel = len(tList)
+
+        ha = "left right".split()
+        va = "bottom top".split()
+        placement_options = list(itertools.product(va, ha))
+
+        #Hide all labels to start
+        for i in range(nLabel):
+            t_i = tList[i]
+            t_i.set_visible(False)
+
+        for i in range(nLabel):
+            t_i = tList[i]
+            #Show this label
+            t_i.set_visible(True)
+
+            if i == 0:
+                continue 
+
+            #See if we need to hide it again
+            # if i == 1:
+            #     idebug()
+            for pl in placement_options:
+                t_i.set_horizontalalignment(pl[1])
+                t_i.set_verticalalignment(pl[0])
+                # print(  t_i.get_text(), pl, t_i.get_ha(), t_i.get_va())
+                t_i.set_visible(True)
+                # plt.pause(1)
+                # print( getTextBbox(t_i))
+                # _x0 = getTextBbox(t_i).x0
+                # assert _x0 > 0
+
+                is_displayable = True
+
+                for j in range(i):
+                    t_j = tList[j]
+
+                    if  t_j.get_visible() and self.overlap(t_i, t_j):
+                        # print(f"{i} overlaps with {j}")
+                        is_displayable = False
+                        #We found an overlap, this placement is invalid, stop looking
+                        #Try the next placement option
+                        break
+
+                if is_displayable:
+                    #We found a legimiate placement, stop looking for more
+                    #Try to place the label i+1
+                    t_i.set_visible(True)
+                    # plt.pause(.1)
+                    # print(t_i)
+                    # import ipdb; ipdb.set_trace()
+                    break  #Don't try anymore placements
+                else:
+                    t_i.set_visible(False)
+
+
 
 
 def getTextBbox(text):
@@ -222,14 +244,18 @@ def getTextBbox(text):
 
     This is called out as a separate function because I expect it will be much more work
     in other plotting toolkits
-
     """
-    return text.get_window_extent()
+
+    visible = text.get_visible()
+    text.set_visible(True)
+    bbox = text.get_window_extent()
+    text.set_visible(visible)
+    return bbox
 
 
 def getPointsPerDataUnit():
     """Compute number of data units per font point size
-    
+
     Useful if you want to draw text at a particular size relative your data points.
     Naturally the scaled value is invalidated once the axis limits are changed
     or the figure size is changed.
@@ -237,9 +263,9 @@ def getPointsPerDataUnit():
     Returns
     -------
     x_points_per_data, y_points_per_data
-        Number of font points per data unit in x and y. 
+        Number of font points per data unit in x and y.
 
-    
+
     Example
     --------
     To create text that is as high as 10 data points in the y-axis Do::
@@ -260,14 +286,14 @@ def getPointsPerDataUnit():
     height_points = height_inches * fig.dpi
 
     axl = plt.axis()
-    yscale_data = axl[3] - axl[2] 
+    yscale_data = axl[3] - axl[2]
     xscale_data = axl[1] - axl[0]
 
     #This isn't a hard requirement, I just haven't thought through
     #the consequences of -ve values yet.
     # assert np.all(np.array([yscale_points, xscale_points, yscale_data, xscale_data]) > 0)
 
-    x_points_per_data = width_points / xscale_data 
+    x_points_per_data = width_points / xscale_data
     y_points_per_data = height_points / yscale_data * 1
     return x_points_per_data, y_points_per_data
 
